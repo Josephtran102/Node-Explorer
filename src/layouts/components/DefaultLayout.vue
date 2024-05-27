07dddb858d5c2dd17ad8f919231bc56287b3c393
Josephtran102 <josephtran0505@gmail.com>
2024-05-28 00:10:53 +0700

    Commit message

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -75,6 +75,7 @@ function selected(route: any, nav: NavLink) {
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
+          <img class="w-12 h-12" src="@/assets/images/favicon.png">
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
             Joseph Tran
           </h1>
